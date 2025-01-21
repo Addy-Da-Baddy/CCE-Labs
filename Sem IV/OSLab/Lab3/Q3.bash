6a9eb156d46a7655d0c3b56806db8596df10f3c0
Adriteyo Das <141396531+Addy-Da-Baddy@users.noreply.github.com>
2025-01-21 16:01:39 +0530

    Add files via upload
    Added Lab3

---
+++ Sem IV/OSLab/Lab3/Q3.bash
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+for file in $(find . -type f -name "*.txt"); do
+    newfile="${file%.txt}.text"
+    
+    mv "$file" "$newfile"
+    
+    echo "Renamed $file to $newfile"
+done
---
+++ Sem IV/OSLab/Lab3/Q6.bash
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Loop through all files in the current directory
+for file in *; do
+    # Check if it's a regular file
+    if [ -f "$file" ]; then
+        echo "Processing file: $file"
+        
+        # Use sed to modify lines where 'ex:' appears at the start of the line
+        # or after a period followed by a space.
+        sed -i -E 's/(^|\.[[:space:]])ex:/\1Example:/g' "$file"
+
+        echo "Modifications done in $file"
+    fi
+done
